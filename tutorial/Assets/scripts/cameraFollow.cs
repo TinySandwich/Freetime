d498b7b677b81cf30ea8d526beafe7f84ceb286a
Leman Russ <Leman Russ>
2016-03-27 14:25:33 -0700

    The camera is now follows the aiming article which helps with aiming and keeps targets on screen while flying around.

--- tutorial/Assets/scripts/cameraFollow.cs
+++ tutorial/Assets/scripts/cameraFollow.cs
@@ -12,11 +12,13 @@ public class cameraFollow : MonoBehaviour {
 	{
 		if (target)
 		{
-			Vector3 point = GetComponent<Camera>().WorldToViewportPoint(target.position);
-			Vector3 delta = target.position - GetComponent<Camera>().ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z)); //(new Vector3(0.5, 0.5, point.z));
+			Vector3 midpoint = (target.position + GetComponent<Camera> ().ScreenToWorldPoint (Input.mousePosition)) / 2;
+			midpoint.z = target.position.z; /*This prevents the camera from changing z levels*/
+
+			Vector3 point = GetComponent<Camera>().WorldToViewportPoint(midpoint);
+			Vector3 delta = midpoint - GetComponent<Camera>().ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z)); //(new Vector3(0.5, 0.5, point.z));
 			Vector3 destination = transform.position + delta;
 			transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
 		}
-
 	}
 }
