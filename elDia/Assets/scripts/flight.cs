90b549c276276c11d9c410d4360801021ac2a97c
Leman Russ <Leman Russ>
2016-05-15 19:10:37 -0700

    The main Character can move!

---
+++ elDia/Assets/scripts/flight.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class flight : MonoBehaviour {
+
+	public float flyspeed = 1.0f;
+	private Vector3 moveDirection = Vector3.zero;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 move = new Vector3(Input.GetAxis ("Horizontal") * flyspeed, Input.GetAxis ("Vertical") * flyspeed, 0);
+
+		transform.position = transform.position + move;
+	}
+}
