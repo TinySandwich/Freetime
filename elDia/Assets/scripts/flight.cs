79371511a515470fe3c8bed5c4a6e9aaa10c8e4d
Leman Russ <Leman Russ>
2016-05-17 19:21:51 -0700

    New Images added to the game

--- elDia/Assets/scripts/flight.cs
+++ elDia/Assets/scripts/flight.cs
@@ -3,7 +3,8 @@ using System.Collections;
 
 public class flight : MonoBehaviour {
 
-	public float flyspeed = 1.0f;
+	public float flyspeed = 4.0f;
+	public int slowDown = 2;
 	private Vector3 moveDirection = Vector3.zero;
 
 	// Use this for initialization
@@ -14,6 +15,9 @@ public class flight : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		Vector3 move = new Vector3(Input.GetAxis ("Horizontal") * flyspeed, Input.GetAxis ("Vertical") * flyspeed, 0);
+		if (move.x < 0) {
+			move.x = move.x * slowDown;
+		}
 
 		transform.position = transform.position + move;
 	}
