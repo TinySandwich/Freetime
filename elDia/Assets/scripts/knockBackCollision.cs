3d8d1ebc3866464dde892688127061af242dd1de
Leman Russ <Leman Russ>
2016-05-25 19:02:48 -0700

    Background cycles awesomely, and the food now has impact over time.

--- elDia/Assets/scripts/knockBackCollision.cs
+++ elDia/Assets/scripts/knockBackCollision.cs
@@ -3,13 +3,18 @@ using System.Collections;
 
 public class knockBackCollision : MonoBehaviour {
 
-	public int kbDist = 100;
+	public int kbDist = 1;
+	private float timeMultiplier = 1;
 
 	void OnCollisionEnter2D(Collision2D coll) {
 		/*Check to see if the object collided with is the player if so. Knock the player back and delete the object.*/
 		if (coll.gameObject.tag == "Player") {
-			/*Keep the position the same except move the player backwards equal to kbDist*/
-			Vector3 kbVec = new Vector3 (coll.gameObject.transform.position.x - kbDist, coll.gameObject.transform.position.y, coll.gameObject.transform.position.z);
+			timeMultiplier = Time.timeSinceLevelLoad / 5;
+
+			/*Keep the position the same except move the player backwards equal to kbDist multiplied by the timeMultiplier*/
+			float knockback = kbDist * timeMultiplier;
+
+			Vector3 kbVec = new Vector3 (coll.gameObject.transform.position.x - knockback, coll.gameObject.transform.position.y, coll.gameObject.transform.position.z);
 			coll.gameObject.transform.position = kbVec;
 
 			Destroy (this.gameObject);
---
+++ elDia/Assets/scripts/scrollingBackground.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class scrollingBackground : MonoBehaviour {
+
+	public float speed = 1.0f; //Base speed of background
+	public float min = 0f; //Lowest speed the background can move. Negative for moving left.
+	public float max = 10f; // Highest speed the background can move.
+	public Camera gameCam;
+	public Transform player;
+
+	public static scrollingBackground current;
+	private float pos = 0;
+
+	// Use this for initialization
+	void Start () {
+		current = this;
+	}
+
+	public void Update() {
+		pos += speed;
+		if (pos > 1.0f) {
+			pos = 0f;
+		}
+
+		//gameObject.GetComponent<Renderer> ().material.mainTextureOffset = new Vector2 ((Time.time * speed) % 1, 0f);
+
+		gameObject.GetComponent<Renderer>().material.mainTextureOffset = new Vector2 (pos, 0);
+	}
+}
